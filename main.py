52cef6762fe6f579400665ccb511cdcfc1ad933c
Hugo Huete <hugo.huete0402@gmail.com>
2022-04-07 18:12:56 -0600

    1. User and passwords prompts are now in the shein_login function. 2. Added the csv file for the image names and converted the data in a dictionery

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from time import sleep
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -8,20 +9,19 @@ def main():
     # open google chrome browser and login in us.shein.com
     driver = webdriver.Chrome()
     driver.implicitly_wait(4)
+    shein_login(driver)
 
-    username = input('Username: ')
-    password = input('Password: ')
-    shein_login(driver, username, password)
-
-    # Get order number
+    # Get order number and go to the link
     order = input("Order No: ")
     shein_orders_url = 'https://us.shein.com/user/orders/detail/' + order
-
     driver.get(shein_orders_url)
 
 
-    # Get urls of the items in x Shein order.
-    items_urls = get_shein_items_urls(driver)
+    # Get urls of the items in x Shein order. Convert the data in a dictionery sku:name from the dataframe
+    data =  pd.read_csv('image_names.csv', sep=';')
+    data = data.to_dict('list')
+    image_names = dict(zip(data['sku'], data['name']))
+    items_urls = get_shein_items_urls(driver, image_names)
 
     # Go to each of the urls and download all the full images as webp.
     for item_url in items_urls:
@@ -37,11 +37,15 @@ def go_to_url(driver, url):
             continue
 
 
-def shein_login(driver, username, password):
+def shein_login(driver):
     """
     Go to shein login page, find text inputs and write user and pass using send_keys
     """
     go_to_url('https://us.shein.com/user/auth/login')
+
+
+    username = input('Username: ')
+    password = input('Password: ')
 
     # find user text input and send username
     user_input = driver.find_element(By.XPATH, '//div[@class="ksLow S-input S-input_suffix"]//*[contains(@aria-label,"Email Address:")]')
@@ -51,14 +55,13 @@ def shein_login(driver, username, password):
     pass_input.send_keys(password)
 
 
-def get_shein_items_urls(driver):
+def get_shein_items_urls(driver, img_names):
     urls = []
+    order_rows = driver.find_elements(By.XPATH, '//table[@class="c-order-detail-table"]//tbody//tr')
 
 
     return urls
-
-
-    
+ 
 
 
 if __name__ == '__main__':
